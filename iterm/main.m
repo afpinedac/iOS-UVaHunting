e83bc936ac8dd0eb27850931597f8335ea1c5ab7
andr3s2 <andr3s2@gmail.com>
2016-04-18 16:31:11 -0500

    test

--- iterm/main.m
+++ iterm/main.m
@@ -1,17 +1,75 @@
-//
-//  main.m
-//  iterm
-//
-//  Created by Andres Pineda on 4/18/16.
-//  Copyright © 2016 Andres Pineda. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+
+
+@interface UVAClient:NSObject
+@end
+
+
+
+@implementation UVAClient:NSObject
+
+ NSString *API_ENDPOINT = @"http://uhunt.felix-halim.net/api/";
+
+-(NSArray * ) readData: (NSString*) url {
+    
+
+    @try{
+    
+    NSError * error = NULL;
+    NSData * data = [NSData dataWithContentsOfURL:[NSURL URLWithString:url]];
+        
+    NSArray * response = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
+        
+        if(error != NULL){
+            NSLog(@"We had an error in the request:  %@", error);
+        }
+        
+        
+    
+   // NSLog(@"%@, %@", [response valueForKey:@"subs"], error);
+    
+        return response;
+        
+    }@catch(NSException * e){
+        NSLog(@"Error making the requests");
     }
-    return 0;
+    
+   
 }
+
+
+-(NSString * ) convertUserNameToID : (NSString*) username {
+            return  [self readData:username];
+}
+
+-(NSArray * ) getSubmissions:(int)userID {
+    
+    NSString * EP = @"http://uhunt.felix-halim.net/api/subs-user/";
+    
+    NSArray* data = [self readData:[NSString stringWithFormat:@"%@%d", EP, userID]];
+    return data;
+}
+
+
+@end
+
+
+
+
+
+int main(){
+    
+    UVAClient * uc = [[UVAClient alloc] init];
+
+    NSArray * response =  [uc getSubmissions:339];
+    
+    for (NSString * key in response) {
+        NSLog(@"%@", [response valueForKey:key]);
+    }
+    
+    
+    
+    
+    NSLog(@"%@", [response valueForKey:@"name" ]);
+ }
