b50a53f2e94ac9733ba02dd8779d7e6729dd5750
andr3s2 <andr3s2@gmail.com>
2016-04-18 11:35:13 -0500

    Initial Commit

---
+++ iterm/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iterm
+//
+//  Created by Andres Pineda on 4/18/16.
+//  Copyright © 2016 Andres Pineda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
